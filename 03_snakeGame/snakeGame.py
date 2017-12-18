84ce08316c1891e6282b8de9f2eab88c24736c58
0421cjy <0421cjy@naver.com>
2017-12-18 16:10:15 +0900

    snake game setting code

--- 03_snakeGame/snakeGame.py
+++ 03_snakeGame/snakeGame.py
@@ -1,1 +1,15 @@
 import sys, pygame
+import random
+
+pygame.init()
+screen = pygame.display.set_mode((500, 400))
+pygame.display.set_caption('snake')
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    pygame.display.update()
